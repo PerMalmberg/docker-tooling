e9919a7b1f38c1b2d0f07e957e77ef7c6edc6b02
Per Malmberg <per.malmberg@gmail.com>
2019-10-11 19:39:14 +0200

    Added uncrustify docker tooling.

---
+++ uncrustify/check_format.sh
@@ -0,0 +1,12 @@
+if [ ! -d "$1" ]; then
+    echo "Must provide a path to the location of uncrustify.cfg as first argument"
+    exit 1
+elif [ ! -d "$2" ]; then 
+    echo "Must provide a source directory as second argument"
+    exit 1
+else
+    docker run --rm \
+               --mount type=bind,source="$1",target="/cfg" \
+               --mount type=bind,source="$2",target="/src" \
+               permalmberg/uncrustify:latest
+fi
---
+++ uncrustify/run_uncrustify.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+find /src -name "*.cpp" > /src/files_to_check.txt
+find /src -name "*.hpp" >> /src/files_to_check.txt
+find /src -name "*.c" >> /src/files_to_check.txt
+find /src -name "*.h" >> /src/files_to_check.txt
+
+/uncrustify -c /cfg/uncrustify.cfg --check -F /src/files_to_check.txt
+code=$?
+
+if [ -f /src/files_to_check.txt ]; then
+    rm /src/files_to_check.txt
+fi
+
+exit $code
